7094a442db7ef015ee6d16165e58fbaa9f6629fb
Chariselyasa123 <ahmad.charis@raharja.info>
2022-06-13 05:41:00 +0700

    Update: README.md

--- README.md
+++ README.md
@@ -3,12 +3,20 @@
 ## Assets
 
 ### Fonts
+
 - [Digital-7](http://www.styleseven.com/)
 - [Orbitron](https://fonts.google.com/specimen/Orbitron?query=Orbitron)
 
-## Description
-This calculator is made using [Web Componenet](https://developer.mozilla.org/en-US/docs/Web/Web_Components). For moredetail about Web Componenet [Click Here](https://developer.mozilla.org/en-US/docs/Web/Web_Components).
+## Features
 
-Why i use Web Componenet? Because I want to learn other ways to build web applications using custom elements with [Web Componenet](https://developer.mozilla.org/en-US/docs/Web/Web_Components)
+- Can use keyboard (Still Buggy) 🐛🐛
+
+## Description
+
+This calculator is made using [Web Component](https://developer.mozilla.org/en-US/docs/Web/Web_Components). For
+more-detail about Web Component [Click Here](https://developer.mozilla.org/en-US/docs/Web/Web_Components).
+
+Why I use Web Component? Because I want to learn other ways to build web applications using custom elements
+with [Web Component](https://developer.mozilla.org/en-US/docs/Web/Web_Components)
 
 By: Ahmad Charis Elyasa Hafidianto
--- assets/js/ServiceCalculator.js
+++ assets/js/ServiceCalculator.js
@@ -31,15 +31,15 @@ export class ServiceCalculator {
 
     }
 
-    setDisplayValue(value){
+    setDisplayValue(value) {
         this.display.setState('value', value);
     }
 
-    setDisplayOperation(operation){
+    setDisplayOperation(operation) {
         this.display.setState('operation', operation);
     }
 
-    getDisplayOperation(){
+    getDisplayOperation() {
         return this.display.getState('operation');
     }
 
@@ -47,24 +47,24 @@ export class ServiceCalculator {
         return value.substring(0, 16);
     }
 
-    inputNumber(number){
-        if(this.currentValue === '0') this.currentValue = number;
+    inputNumber(number) {
+        if (this.currentValue === '0') this.currentValue = number;
         else this.currentValue = this.value(this.currentValue + number);
     }
 
-    inputOperator(operator){
+    inputOperator(operator) {
         this.prevValue = this.currentValue;
         this.operator = operator;
         this.currentValue = '';
-        this.setDisplayOperation(this.prevValue  +  this.operator);
+        this.setDisplayOperation(this.prevValue + this.operator);
     }
 
-    inputDecimal(dot){
+    inputDecimal(dot) {
         this.currentValue += dot;
     }
 
-    setCurrentValue(){
-        for(const number of this.numbers){
+    setCurrentValue() {
+        for (const number of this.numbers) {
             number.addEventListener('click', () => {
                 this.inputNumber(number.getAttribute('value'));
                 this.setDisplayValue(this.currentValue);
@@ -72,46 +72,46 @@ export class ServiceCalculator {
         }
     }
 
-    setOperator(){
-        for(const operator of this.operators){
+    setOperator() {
+        for (const operator of this.operators) {
             operator.addEventListener('click', () => {
                 this.operatorFn(operator.getAttribute('value'));
             })
         }
     }
 
-    decimalListener(){
+    decimalListener() {
         this.decimal.addEventListener('click', () => {
             this.inputDecimal('.');
         })
     }
 
-    percentListener(){
+    percentListener() {
         this.percent.addEventListener('click', () => {
             this.percentFn();
         })
     }
 
-    powerListener(){
+    powerListener() {
         this.power.addEventListener('click', () => {
             this.powerFn();
         })
     }
 
-    equalSignListener(){
+    equalSignListener() {
         this.equalSign.addEventListener('click', () => {
             this.equals();
         })
     }
 
-    equals(){
+    equals() {
         this.calculate();
         const currentDisplayOperation = this.getDisplayOperation();
         this.setDisplayOperation(currentDisplayOperation + this.currentValue + '=');
     }
 
-    operatorFn(value){
-        if(this.operatorClicked > 0){
+    operatorFn(value) {
+        if (this.operatorClicked > 0) {
             this.calculate()
             this.currentValue = this.total;
         }
@@ -119,54 +119,53 @@ export class ServiceCalculator {
         this.operatorClicked++;
     }
 
-    percentFn(){
-        if(this.prevValue !== ''){
+    percentFn() {
+        if (this.prevValue !== '') {
             this.currentValue = parseFloat(this.currentValue) / 100;
             this.setDisplayValue(this.currentValue);
-        }else{
+        } else {
             this.currentValue /= 100;
             this.setDisplayValue(this.currentValue);
             this.setDisplayOperation(this.currentValue);
         }
     }
 
-    powerFn(){
-        if(this.prevValue !== ''){
+    powerFn() {
+        if (this.prevValue !== '') {
             this.currentValue = Math.pow(parseFloat(this.currentValue), 2);
             this.setDisplayValue(this.currentValue);
-        }else{
+        } else {
             this.currentValue = Math.pow(this.currentValue, 2);
             this.setDisplayValue(this.currentValue);
             this.setDisplayOperation(this.currentValue);
         }
     }
 
-    keyboardListener(){
-        const $this = this;
+    keyboardListener() {
         window.addEventListener('keydown', (e) => {
-            if(this.allowedKey.includes(e.key)) {
+            if (this.allowedKey.includes(e.key)) {
                 switch (e.key) {
                     case 'Enter':
-                        $this.equals();
+                        this.equals();
                         break;
                     case 'Backspace':
-                        $this.currentValue = String($this.currentValue).slice(0, -1);
-                        $this.setDisplayValue($this.currentValue);
+                        this.currentValue = String(this.currentValue).slice(0, -1);
+                        this.setDisplayValue(this.currentValue);
                         break;
                     case '+':
                     case '-':
-                    case 'x':
+                    case '*':
                     case '/':
-                        $this.operatorFn(e.key);
+                        this.operatorFn(e.key);
                         break;
                     case '.':
-                        $this.inputDecimal(e.key);
+                        this.inputDecimal(e.key);
                         break;
                     case '%':
-                        $this.percentFn();
+                        this.percentFn();
                         break;
                     case '^':
-                        $this.powerFn();
+                        this.powerFn();
                         break;
                     default:
                         this.inputNumber(e.key);
@@ -177,7 +176,7 @@ export class ServiceCalculator {
         })
     }
 
-    calculate(){
+    calculate() {
         switch (this.operator) {
             case '+':
                 this.total = parseFloat(this.prevValue) + parseFloat(this.currentValue);
@@ -198,7 +197,7 @@ export class ServiceCalculator {
         this.setDisplayValue(this.total);
     }
 
-    clearValue(){
+    clearValue() {
         this.allClear.addEventListener('click', () => {
             this.currentValue = '';
             this.prevValue = '';
